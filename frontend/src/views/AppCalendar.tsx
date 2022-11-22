d400b8004a76c00d7b17a21cde95b798a109126d
kjw <yoonlight12@gmail.com>
2022-11-22 20:44:52 +0900

    feat(component): apply api with calendar
    Refs: AFSY-35

--- frontend/src/views/AppCalendar.tsx
+++ frontend/src/views/AppCalendar.tsx
@@ -4,14 +4,42 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import "./app-calendar.scss";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import { placeApi } from "../apis/placeApi";
 
 // [ ]: customize 좌우 버튼 hover
 export const AppCalendar = () => {
   const navigate = useNavigate();
+  const [place, setPlace] = useState(0);
+  const calendarRef = useRef<FullCalendar>(null);
+
+  useEffect(() => {
+    placeApi.getUserPlaces(1).then((res) => {
+      // [ ]: tab으로 place 여러개일 경우 여러개 출력
+      setPlace(res.data[0].id);
+    });
+  }, []);
+
+  // [ ]: fullcalendar issue 발생하는지 확인 필요
+  // Reference: https://stackoverflow.com/questions/72119019/why-does-useeffect-react-hook-not-work-properly-with-dependency
+  useEffect(() => {
+    if (place && calendarRef.current) {
+      const calendarApi = calendarRef.current.getApi();
+      placeApi.getPlaceReservations(place).then((res) => {
+        // [ ]: 중복 코드 리팩토링
+        const events = res.data.map((event) => {
+          return { start: event.bookingStart, end: event.bookingEnd };
+        });
+        calendarApi.addEventSource(events);
+      });
+    }
+  }, [place]);
+
   return (
     <div className="container">
       <div className="wrapper">
         <FullCalendar
+          ref={calendarRef}
           buttonIcons={{
             prev: "chevron-left",
             next: "chevron-right",
@@ -26,20 +54,8 @@ export const AppCalendar = () => {
           dayMaxEvents={true}
           selectMirror={true}
           dateClick={(info) => {
-            console.log(info);
             navigate(`/calendar/${info.dateStr}`);
-            //
           }}
-          events={[
-            {
-              title: "aou",
-              start: "2022-11-20T03:00:00",
-              end: "2022-11-20T06:00:00",
-            },
-            { title: "aou", date: "2022-11-20T00:00:00" },
-            { title: "aou", date: "2022-11-20T00:00:00" },
-            { title: "aou", date: "2022-11-20T00:00:00" },
-          ]}
         />
       </div>
     </div>
