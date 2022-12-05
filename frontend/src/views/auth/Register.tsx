2b7139d5f4403d912816e62deb85d9568070c850
kjw <yoonlight12@gmail.com>
2022-12-05 16:39:40 +0900

    feat(views): add auth (AFSY-41)
    create email, login, register, reset page

---
+++ frontend/src/views/auth/Register.tsx
@@ -0,0 +1,93 @@
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState, useEffect } from "react";
+import KakaoLogin from "react-kakao-login";
+import { Link, useNavigate } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import { useAuthActions } from "../../features/auth/auth.action";
+import { ReactComponent as KakaoSvg } from "../../assets/kakao-svgrepo-com.svg";
+import { authState } from "../../features/auth/auth.state";
+
+function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [auth, _] = useRecoilState(authState);
+  const navigate = useNavigate();
+  const authActions = useAuthActions();
+  const token = import.meta.env.VITE_KAKAO_KEY;
+
+  const register = () => {
+    if (!name) alert("Please enter name");
+    authActions.registerWithEmailAndPassword(name, email, password);
+  };
+
+  useEffect(() => {
+    if (auth) navigate("/");
+  }, [auth]);
+
+  return (
+    <div className="auth">
+      <div className="container">
+        <input
+          type="text"
+          className="auth__input"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="이름을 입력해주세요."
+        />
+        <input
+          type="email"
+          className="auth__input"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="example@example.com"
+        />
+        <input
+          type="password"
+          className="auth__input"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="8자 이상, 영문, 숫자, 특수문자"
+        />
+        <button className="auth__btn" onClick={register}>
+          회원가입
+        </button>
+        <button
+          className="auth__btn auth__btn-google auth__btn-icon"
+          onClick={authActions.signInWithGoogle}
+        >
+          <div className="auth__icon">
+            <FontAwesomeIcon icon={faGoogle} />
+          </div>
+          구글로 시작
+        </button>
+        <KakaoLogin
+          token={token}
+          onSuccess={(res) => {
+            authActions.loginWithKakao(res.response.access_token);
+          }}
+          onFail={console.error}
+          onLogout={console.info}
+          render={({ onClick }) => {
+            return (
+              <button
+                className="auth__btn auth__btn-kakao auth__btn-icon"
+                onClick={onClick}
+              >
+                <div className="auth__icon">
+                  <KakaoSvg />
+                </div>
+                카카오로 시작
+              </button>
+            );
+          }}
+        />
+        <div className="auth__link">
+          <Link to="/login">로그인</Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Register;
