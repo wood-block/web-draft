777c4cda865f58901f043e68935799f5a0df848e
kjw <yoonlight12@gmail.com>
2022-12-05 16:47:06 +0900

    feat(components): add logout to header (AFSY-41)

--- frontend/src/components/AppHeader.tsx
+++ frontend/src/components/AppHeader.tsx
@@ -1,25 +1,32 @@
 import "./header.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch, faUser } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowRightFromBracket,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+import { useAuthActions } from "../features/auth/auth.action";
+import { Link } from "react-router-dom";
 
 export const AppHeader = () => {
+  const authActions = useAuthActions();
   return (
-    <header className="header">
+    <header className="header header-sticky">
       <div className="header__inner">
         <div className="header-user">
-          <a className="header__link" href="login">
+          <Link className="header__link" to={"/login"}>
             <FontAwesomeIcon icon={faUser} className="header__icon" />
-          </a>
+          </Link>
         </div>
         <div className="header-title">
           <a className="header-title__link" href="">
             뮤지컬팝스오케스트라
           </a>
         </div>
-        <div className="header-search">
-          <a className="header__link" href="login">
-            <FontAwesomeIcon icon={faSearch} className="header__icon" />
-          </a>
+        <div className="header-search" onClick={authActions.logout}>
+          <FontAwesomeIcon
+            icon={faArrowRightFromBracket}
+            className="header__icon"
+          />
         </div>
       </div>
     </header>
