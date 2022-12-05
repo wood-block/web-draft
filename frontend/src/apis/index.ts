e472e01769fe684acbbc7c9353dd4cc8d245ebe4
kjw <yoonlight12@gmail.com>
2022-12-05 16:33:58 +0900

    feat: add jwt to api header (AFSY-41)

---
+++ frontend/src/apis/index.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { useRecoilState } from "recoil";
+import { authState } from "../features/auth/auth.state";
+
+export const useApi = () => {
+  const [auth, setAuth] = useRecoilState(authState);
+
+  if (auth) {
+    return axios.create({
+      baseURL: "/api",
+      headers: {
+        Authorization: `Bearer ${auth}`,
+      },
+    });
+  }
+  return axios.create({ baseURL: "/api" });
+};
